d9c857ab02ac4793bfb0b931228876662e96be30
ngwszsd <45424459+ngwszsd@users.noreply.github.com>
2021-12-06 10:22:37 +0800

    publish

---
+++ blogs/React/Hooks/reduce.md
@@ -0,0 +1,16 @@
+---
+title: reduce方法
+date: 2021-12-3
+tags:
+ - tagReact
+categories:
+ - React
+---
+## 迭代[MDN](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce)
+- eg1
+```js
+[0, 1, 2, 3, 4].reduce((accumulator, currentValue, currentIndex, array) => {
+    return accumulator + currentValue
+}, 10)
+```
+![详细迭代值](https://s3.bmp.ovh/imgs/2021/12/8a105412a269b580.png)
--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@ git add -A
 git commit -m "publish"
 git push -f git@github.com:ngwszsd/blog-github.git main
 
-npm run build
-cd public
-git init 
-git branch -M main
-git add -A
-git commit -m 'publish status'
-git push -f git@github.com:ngwszsd/ngwszsd.github.io.git main
+# 使用了cf page/ netlify 等构建工具
+# npm run build
+# cd public
+# git init 
+# git branch -M main
+# git add -A
+# git commit -m 'publish status'
+# git push -f git@github.com:ngwszsd/ngwszsd.github.io.git main
